894964c6e8710a30a0947d3272d049d97c2011b4
rjan939 <dg47gamer@gmail.com>
2024-07-15 12:57:30 -0500

    modified transposer to print flats if input was flat otherwise print sharp or natural

--- transposer.c
+++ transposer.c
@@ -7,37 +7,24 @@
 #define NUM_NOTES 12
 
 char *notes[NUM_NOTES] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
-
-// Convert flat to sharp note to simplify a little
-char *flat_to_sharp(char *note) {
-  if (strcmp(note, "Db") == 0) return "C#";
-  if (strcmp(note, "Eb") == 0) return "D#";
-  if (strcmp(note, "Fb") == 0) return "E";
-  if (strcmp(note, "Gb") == 0) return "F#";
-  if (strcmp(note, "Ab") == 0) return "G#";
-  if (strcmp(note, "Bb") == 0) return "A#";
-  if (strcmp(note, "Cb") == 0) return "B";
-  return note;
-}
+char *flats[NUM_NOTES] = {"C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"};
 
 int find_note_index(char *note) {
   for (int i = 0; i < NUM_NOTES; i++) {
-    if (strcmp(notes[i], note) == 0) {
-      return i;
-    }
+    if ((strcmp(notes[i], note) == 0) || (strcmp(flats[i], note)) == 0) 
+      return i; 
   }
   return -1;
 }
 
-char *transpose_note(char *note, int semitones) {
-  note = flat_to_sharp(note);
+char *transpose_note(char *note, int semitones, bool flat) {
   int note_index = find_note_index(note);
   if (note_index == -1) {
     printf("Invalid note: %s\n", note);
     exit(1);
   }
   int transposed_index = (note_index + semitones + NUM_NOTES) % NUM_NOTES;
-  return notes[transposed_index]; 
+  return (flat) ? flats[transposed_index] : notes[transposed_index]; 
 }
 
 
@@ -75,7 +62,10 @@ int main(int argc, char **argv) {
   printf("Transposed sequence: ");
   for (int i = start; i < argc; i++) {
     char *note = argv[i];
-    char *transposed_note = transpose_note(note, semitones);
+    bool flat = false;
+    if (note[1] == 'b')
+      flat = true;
+    char *transposed_note = transpose_note(note, semitones, flat);
     printf("%s ", transposed_note);
   }
   printf("\n");
